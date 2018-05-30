c349deec9072831462d87a1b9c5518754b763413
ClaireBvs <clairembeauvais@gmail.com>
2018-05-30 09:29:20 -0600

    implements method base cost for an activity

--- lib/activity.rb
+++ lib/activity.rb
@@ -1,13 +1,17 @@
 class Activity
   attr_reader :name,
-              :participants
+              :participants,
+              :base_cost
 
-  def initialize(name)
+  def initialize(name, base_cost = 50)
     @name = name
     @participants = {}
+    @base_cost = base_cost
   end
 
   def add_participants(name, cost)
     @participants[name] = cost
-  end 
+  end
+
+
 end
--- test/activity_test.rb
+++ test/activity_test.rb
@@ -27,8 +27,22 @@ class ActivityTest < Minitest::Test
 
   def test_can_add_participants_to_activity
     activity = Activity.new("hiking")
+
     activity.add_participants("George", 20)
     activity.add_participants("Louis", 40)
-    assert_equal ({"George" => 20, "Louis" => 40}), activity.participants 
+
+    assert_equal ({"George" => 20, "Louis" => 40}), activity.participants
+  end
+
+  def test_activity_has_a_base_cost_of_50_by_default
+    activity = Activity.new("hiking")
+
+    assert_equal 50, activity.base_cost
+  end
+
+  def test_expensive_activity_have_different_base_cost
+    activity = Activity.new("stand up paddling", 100)
+
+    assert_equal 100, activity.base_cost
   end
 end
