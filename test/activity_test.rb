874b3c3c4caf4c3ae6e73a17e34d7559e475f538
ClaireBvs <clairembeauvais@gmail.com>
2018-05-30 09:14:01 -0600

    implements method name for activity class

--- test/activity_test.rb
+++ test/activity_test.rb
@@ -4,4 +4,20 @@ require 'minitest/pride'
 require './lib/activity'
 
 class ActivityTest < Minitest::Test
+
+  def test_it_exists
+    activity = Activity.new("hiking")
+    assert_instance_of Activity, activity
+  end
+
+  def test_activity_has_name
+    activity = Activity.new("hiking")
+    assert_equal "hiking", activity.name
+  end
+
+  def test_can_have_different_activity_name
+    activity = Activity.new("stand up paddling")
+    assert_equal "stand up paddling", activity.name
+  end
+
 end
