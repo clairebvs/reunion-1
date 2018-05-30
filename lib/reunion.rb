d412ddb2cebbe2411f708341b9b568d6d45b5d96
ClaireBvs <clairembeauvais@gmail.com>
2018-05-30 10:27:47 -0600

    implements reunion class and method location and activities

--- lib/reunion.rb
+++ lib/reunion.rb
@@ -1,2 +1,14 @@
 class Reunion
+  attr_reader :location,
+              :activities
+
+  def initialize(location = "Lake Tahoe")
+    @location = location
+    @activities = []
+  end
+
+  def add_activities(activity)
+    activity = Activity.new("hiking")
+    @activities << activity
+  end
 end
--- test/reunion_test.rb
+++ test/reunion_test.rb
@@ -2,6 +2,31 @@ require 'minitest'
 require 'minitest/test'
 require 'minitest/autorun'
 require './lib/reunion'
+require './lib/activity'
 
 class ReunionTest < Minitest::Test
+
+  def test_it_exists
+    reunion = Reunion.new
+    assert_instance_of Reunion, reunion
+  end
+
+  def test_reunion_has_location_lake_tahoe_by_default
+    reunion = Reunion.new
+    assert_equal "Lake Tahoe", reunion.location
+  end
+
+  def test_reunion_has_no_activity_when_start
+    reunion = Reunion.new
+
+    assert_equal [], reunion.activities
+  end
+
+  def test_cann_add_activities_to_reunion
+    reunion = Reunion.new
+    activity = Activity.new("hiking")
+    reunion.add_activities(activity)
+
+    assert_equal [activity], reunion.activities
+  end
 end
